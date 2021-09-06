85723104781c0f820f0d46b61959b633e6fc18c1
seabluemaker <seabluemaker@gmail.com>
2021-09-06 17:04:36 +0900

    Create: Create Search Bar

---
+++ src/components/common/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Input({ type, placeholder = "" }) {
+	return (
+		<input type={type} placeholder={placeholder} />
+	);
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
