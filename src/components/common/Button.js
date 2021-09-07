da186958add095b7345f09d83d00e3d98f5dd577
seabluemaker <seabluemaker@gmail.com>
2021-09-07 14:33:55 +0900

    Create: Create user menu modal

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,9 +1,8 @@
-import React from "react";
 import PropTypes from "prop-types";
 
-export default function Button({ type, content, onClick = () => {} }) {
+export default function Button({ className, type, content, onClick = () => {} }) {
 	return (
-		<button type={type} onClick={onClick}>
+		<button className={className} type={type} onClick={onClick}>
 			{content}
 		</button>
 	);
---
+++ src/constants/backgroundColor.js
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+const mauve = css`
+  background-color: rgba(174, 165, 223, 0.7);
+`;
+
+export const backgroundColor = {
+  mauve,
+};
