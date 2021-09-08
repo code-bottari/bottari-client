2b6acd5457a55eb99949675ac9d2b18c166d094a
seabluemaker <seabluemaker@gmail.com>
2021-09-08 21:09:35 +0900

    Create: Create sort selectbox of snippet list

---
+++ src/components/SelectBox.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+
+const Select = styled.select`
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  width: 120px;
+  height: 30px;
+  padding: 5px 30px 5px 10px;
+  border: 2px solid #543FD3;
+  border-radius: 4px;
+  outline: 0 none;
+  background: url('https://freepikpsd.com/media/2019/10/down-arrow-icon-png-7-Transparent-Images.png') calc(100% - 5px) center no-repeat;
+  background-size: 20px;
+`;
+
+const SelectOption = styled.option`
+  padding: 3px 0;
+  background: black;
+  color: #fff;
+`;
+
+export default function SelectBox() {
+  const [selected, setSelected] = useState("default");
+
+  const handleChangeSelect = (event) => {
+    setSelected(event.target.value);
+  };
+
+  return (
+    <Select name="fruits" className="select" onChange={handleChangeSelect} value={selected}>
+      <SelectOption value="default" disabled>정렬 순서</SelectOption>
+      <SelectOption value="latest">최신순</SelectOption>
+      <SelectOption value="like">좋아요 많은 순</SelectOption>
+      <SelectOption value="comment">댓글 많은 순</SelectOption>
+    </Select>
+  );
+}
