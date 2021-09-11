9f4ac2788b90d3511b0765c95b728f8ddbdc98b3
HannaChoi <hn4880@gmail.com>
2021-09-12 02:02:46 +0900

    Create: Create dropdown confirming delete

--- src/constants/methods.js
+++ src/constants/methods.js
@@ -1,6 +1,7 @@
 const METHODS = {
   GET: "GET",
   POST: "POST",
+  DELETE: "DELETE",
 };
 
 export default METHODS;
--- src/constants/names.js
+++ src/constants/names.js
@@ -1,5 +1,7 @@
 const NAMES = {
   REGISTRATION: "등록",
+  CANCEL: "취소",
+  DELETE: "삭제",
 };
 
 export default NAMES;
