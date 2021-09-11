70dbca08b5acbfddcbb4303b161d091499211fee
seabluemaker <seabluemaker@gmail.com>
2021-09-11 12:47:18 +0900

    Create: Create snippet list and modify sippet margin

---
+++ src/components/SnippetList.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+import Snippet from "./Snippet";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ListBox = styled.div`
+  width: 700px;
+`;
+
+export default function SnippetList({ snippets }) {
+  return (
+    <Wrapper>
+      <ListBox>
+        {snippets.map((data) => (
+          <Snippet data={data} />
+        ))}
+      </ListBox>
+    </Wrapper>
+  );
+}
