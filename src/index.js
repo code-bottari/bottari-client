774b1a0ff2618e829632e26c2f5649a0c180a81d
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-13 00:43:39 +0900

    Feat: Connect socket.io

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./config/firebase";
+import "./config/socketId";
 
 ReactDOM.render(
   <React.StrictMode>
