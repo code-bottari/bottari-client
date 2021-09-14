cc935e8c0cbf004a523796278c720c61c6b16610
HannaChoi <hn4880@gmail.com>
2021-09-15 02:48:17 +0900

    Feat: Connect react-query

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import GlobalStyle from "./constants/styledComponent/GlobalStyle";
 import AppHeader from "./components/AppHeader/AppHeader";
 import Main from "./components/Main/Main";
 import SnippetDetailPage from "./components/SnippetDetailPage/SnippetDetailPage";
+import RegisterCard from "./components/RegisterCard/RegisterCard";
 import UserInformation from "./components/UserInformationPage/UserInformation";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <GlobalStyle />
-      <AppHeader />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/snippets/:id" component={SnippetDetailPage} />
-        <Route path="/users/:id" component={UserInformation}/>
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <GlobalStyle />
+        <AppHeader />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/snippets/:id" component={SnippetDetailPage} />
+          <Route path="/users/register" component={RegisterCard} />
+          <Route path="/users/:id" component={UserInformation}/>
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 }
