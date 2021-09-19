c00d2eb2c75abe25a498366fe444ad382fad5973
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-20 01:50:50 +0900

    Refactor: Add notification list click event handler

--- src/components/AppHeader/UserTools/NotificationButton.js
+++ src/components/AppHeader/UserTools/NotificationButton.js
@@ -1,121 +1,54 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
-import styled from "styled-components";
 
 import Button from "../../common/Button";
-import Dropdown from "../../common/Dropdown";
 
-import { getNotificationList } from "../../../api/service";
+import NotificationDropdown from "./NotificationDropdown";
+
+import { ICON } from "../../../constants/variants";
 
 import {
-  ICON,
-  LIST,
-  NOTIFICATION
-} from "../../../constants/variants";
+  ON_NOTIFICATION_ICON,
+  OFF_NOTIFICATION_ICON,
+} from "../../../constants/images";
 
-const NotificationWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const DropdownWrapper = styled.div`
-  position: absolute;
-  z-index: 1;
-  top: 80px;
-  right: 15px;
-`;
-
-const Message = styled.p`
-  display: inline-block;
-  margin: 0px;
-`;
-
-const selectPath = (type, targetPath) => {
-  const pathTypes = {
-    like: `/snippets/${targetPath}`,
-    comment: `/snippets/${targetPath}`,
-    snippet: `/snippets/${targetPath}`,
-    follower: `/users/${targetPath}`,
-  };
-
-  return pathTypes[type];
-};
-
-const selectIcon = (type) => {
-  const iconTypes = {
-    like: "images/like.png",
-    comment: "images/comment.png",
-    snippet: "images/new_snippet.png",
-    follower: "images/followMark.png",
-  };
-
-  return iconTypes[type];
-};
-
-const selectAlternative = (type) => {
-  const alternativeTypes = {
-    like: "좋아요 아이콘",
-    comment: "댓글 아이콘",
-    snippet: "스니펫 아이콘",
-    follower: "팔로우 아이콘",
-  };
-
-  return alternativeTypes[type];
-};
-
-const selectMessage = (type, user) => {
-  const messageTypes = {
-    like: `${user.nickname}님께서 회원님의 스니펫을 좋아합니다.`,
-    comment: `${user.nickname}님께서 회원님의 스니펫에 댓글을 남겼습니다.`,
-    snippet: `${user.nickname}님께서 새 스니펫을 업로드하셨습니다.`,
-    follower: `${user.nickname}님께서 회원님을 팔로우하셨습니다.`,
-  };
-
-  return messageTypes[type];
-};
+import { getNotificationList } from "../../../api/service";
 
 export default function NotificationButton() {
   const [isClicked, setIsClicked] = useState(false);
 
-  const { data } = useQuery("notificationList", async () => await getNotificationList());
+  const handleDropdown = () => setIsClicked(!isClicked);
 
-  const filteredNotificationList = data?.notificationList.filter((notification) => notification.isChecked === false);
+  const { isLoading, data } = useQuery("notificationList", async () => await getNotificationList());
 
-  const handleButtonClick = () => setIsClicked(!isClicked);
+  if (isLoading) {
+    return <></>;
+  }
 
-  const handleDropdownClick = ({ target, currentTarget }) => {
-    if (target === currentTarget) {
-      return;
-    }
+  const filteredNotificationList = data.notificationList.filter((notification) => notification.isChecked === false);
+  const hasNotification = !!filteredNotificationList.length;
 
-    setIsClicked(!isClicked);
-  };
+  const notificationIcon = hasNotification ? ON_NOTIFICATION_ICON : OFF_NOTIFICATION_ICON;
 
   return (
-    <NotificationWrapper>
-      <Button variant={ICON} onClick={handleButtonClick}>
-        <img src="images/notification_icon.png" alt="알림 버튼" width="40px" height="35px" />
-      </Button>
-      <DropdownWrapper>
-        {isClicked && (
-          <Dropdown variant={LIST} onClick={handleDropdownClick}>
-            {filteredNotificationList.map(({ type, user, targetId: targetPath, _id: notificationId }) => (
-              <Link to={selectPath(type, targetPath)}>
-                <Button
-                  variant={NOTIFICATION}
-                  key={notificationId}
-                >
-                  <img src={selectIcon(type)} alt={selectAlternative(type)} width="30px" height="25px" />
-                  <Message>
-                    {selectMessage(type, user, targetPath)}
-                  </Message>
-                </Button>
-              </Link>
-            ))}
-          </Dropdown>
+    <>
+      <Button
+        variant={ICON}
+        onClick={handleDropdown}
+        children={(
+          <img
+            src={notificationIcon}
+            alt="알림 버튼"
+            width="40px"
+            height="35px"
+          />
         )}
-      </DropdownWrapper>
-    </NotificationWrapper>
+      />
+      {isClicked && (
+        <NotificationDropdown
+          notificationList={filteredNotificationList}
+          onClick={handleDropdown}
+        />)}
+    </>
   );
 }
---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+const ON_NOTIFICATION_ICON = "/images/on_notification.png";
+const OFF_NOTIFICATION_ICON = "/images/off_notification.png";
+const LIKE_ICON = "/images/like.png";
+const COMMENT_ICON = "/images/comment.png";
+const NEW_SNIPPET_ICON = "/images/new_snippet.png";
+const FOLLOWER_ICON = "/images/followMark.png";
+
+export {
+  ON_NOTIFICATION_ICON,
+  OFF_NOTIFICATION_ICON,
+  LIKE_ICON,
+  COMMENT_ICON,
+  NEW_SNIPPET_ICON,
+  FOLLOWER_ICON,
+};
